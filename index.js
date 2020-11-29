1a8cdd71ee22c182850634c00e4b72dd09bd71eb
ellenupark <ellenupark@gmail.com>
2020-11-28 19:13:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+function map(arr, callback) {
+    return arr.map(e => callback(e));
+};
+
+function reduce(arr, callback, start) {
+    let s = !!start ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+        s = callback(arr[i], s)
+    }
+
+    return s;
+}
